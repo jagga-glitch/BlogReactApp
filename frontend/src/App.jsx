6b88683e548e411b4d254d32d556b08074d39a30
Jigyasa <guptajigyasa893@gmail.com>
2026-06-17 14:26:39 +0530

    initial build

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>
+     <h1>AI Blogs</h1> 
+      <ul>
+        <li>AI Ethics</li>
+        <li>Machine Learning</li>
+        <li>Neural Networks</li>
+        <li>Natural Language Processing</li>
+        <li>Computer Vision</li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
